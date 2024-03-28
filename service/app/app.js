fb871180776ef63a07989ca61949b0a2cea71322
Malik Muhammad Huzaifa <huzaifa.ma@northeastern.edu>
2024-03-28 10:41:42 -0400

    Add server and app initialization code

---
+++ service/app/app.js
@@ -0,0 +1,23 @@
+import express from "express";
+import cors from "cors";
+import mongoose from "mongoose";
+import ininializeRoutes from "./routes/index.js";
+
+const init = (app) => {
+  const MONGO_URI = process.env.MONGO_CONNECTION_STRING;
+
+  app.use(cors());
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: true }));
+
+  mongoose.connect(MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+
+  console.log("Connected to MongoDB", MONGO_URI);
+
+  ininializeRoutes(app);
+};
+
+export default init;
---
+++ service/server.js
@@ -0,0 +1,13 @@
+import express from "express";
+import dotenv from "dotenv";
+import init from "./app/app.js";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT;
+init(app);
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
