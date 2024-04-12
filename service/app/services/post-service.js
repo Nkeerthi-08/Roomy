9c1d124dae1c58c56cf0935cd07677854e606949
Malik Muhammad Huzaifa <huzaifa.ma@northeastern.edu>
2024-04-12 19:02:20 -0400

    Add Azure Blob Storage integration for photo uploads

--- service/app/services/post-service.js
+++ service/app/services/post-service.js
@@ -1,7 +1,18 @@
 import Post from "../models/post.js";
+import { BlobServiceClient } from "@azure/storage-blob";
 
+/**
+ * Creates a new post.
+ * @param {Object} post - The post object containing the post details.
+ * @returns {Promise<Object>} - A promise that resolves to an object with a success message.
+ * @throws {Error} - If the post is not created.
+ */
 export const createPost = async (post) => {
-  const res = await Post.create(post);
+  const photos = post.photos;
+  const res = new Post(post);
+  const photoUrls = await uploadPhotos(res._id, photos);
+  res.photos = photoUrls;
+  await res.save();
 
   if (!res) {
     throw new Error("Post not created");
@@ -60,6 +71,14 @@ export const deletePost = async (id) => {
   return { message: "Post deleted", success: true };
 };
 
+/**
+ * Approves a post.
+ *
+ * @param {string} id - The ID of the post to be approved.
+ * @param {object} approvedBy - The user who is approving the post.
+ * @returns {Promise<object>} - A promise that resolves to an object containing the approval status and message.
+ * @throws {Error} - If the user is not an admin or if the post is not approved.
+ */
 export const approvePost = async (id, approvedBy) => {
   // only admin can approve posts
   if (approvedBy.role !== "admin") {
@@ -76,4 +95,37 @@ export const approvePost = async (id, approvedBy) => {
   }
 
   return { message: "Post approved", success: true };
+};
+
+/**
+ * Uploads photos to Azure Blob Storage.
+ *
+ * @param {string} id - The ID of the post.
+ * @param {Array} photos - An array of photo objects.
+ * @returns {Promise<Array>} - A promise that resolves to an array of photo URLs.
+ * @throws {Error} - If there is an error uploading the photos.
+ */
+export const uploadPhotos = async (id, photos) => {
+  try {
+    // Establishes a connection with Azure Blob Storage
+    const blobServiceClient = new BlobServiceClient(
+      process.env.BLOB_SERVICE_URL
+    );
+    const containerClient = blobServiceClient.getContainerClient(
+      process.env.AZURE_STORAGE_CONTAINER_NAME
+    );
+
+    // upload each photo to the blob storage
+    const photoUrls = [];
+    for (const photo of photos) {
+      const blobName = `${id}-${photo.originalname}`;
+      const blockBlobClient = containerClient.getBlockBlobClient(blobName);
+      await blockBlobClient.upload(photo.buffer, photo.size);
+      photoUrls.push({ url: blockBlobClient.url });
+    }
+
+    return photoUrls;
+  } catch (error) {
+    throw new Error(`Error uploading photos: ${error.message}`);
+  }
 };
