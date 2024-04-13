ab5bd5148e529b1b73a8c9c32b2b73a7ff0abafa
Malik Muhammad Huzaifa <huzaifa.ma@northeastern.edu>
2024-04-12 23:56:16 -0400

    Fix commented out code for sending email in azureUtils.js

--- service/app/utils/azureUtils.js
+++ service/app/utils/azureUtils.js
@@ -57,10 +57,10 @@ export const sendEmail = async (emails, subject, message) => {
       },
     };
 
-    const poller = await emailClient.beginSend(emailMessage);
-    const result = await poller.pollUntilDone();
+    // const poller = await emailClient.beginSend(emailMessage);
+    // const result = await poller.pollUntilDone();
 
-    console.log("Email sent with message id: ", result);
+    // console.log("Email sent with message id: ", result);
   } catch (error) {
     console.error(`Error sending email: ${error.message}`);
   }
