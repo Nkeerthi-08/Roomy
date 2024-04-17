5afaf8b9be961e8793a0ee301010649d67ca2db9
Malik Muhammad Huzaifa <huzaifa.ma@northeastern.edu>
2024-04-17 19:09:18 -0400

    Add morgan middleware and logger utility

--- service/app/app.js
+++ service/app/app.js
@@ -4,14 +4,24 @@ import mongoose from 'mongoose';
 import ininializeRoutes from './routes/index.js';
 import passport from 'passport';
 import { initPassport } from './middlewares/passport-config.js';
+import morganMiddleware from './middlewares/morgan.middleware.js';
 
 const init = (app) => {
   const MONGO_URI = process.env.MONGO_CONNECTION_STRING;
 
   initPassport(passport);
 
+  // Add the morgan middleware
+  app.use(morganMiddleware);
   app.use(cors());
-  app.use(express.json());
+  app.use(
+    express.json({
+      limit: '5mb',
+      verify: (req, res, buf) => {
+        req.rawBody = buf.toString();
+      },
+    })
+  );
   app.use(express.urlencoded({ extended: true }));
   app.use(passport.initialize());
 
